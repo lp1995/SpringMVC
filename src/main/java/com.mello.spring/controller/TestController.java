a858047c29582a7f87dea3091013b067f813dc2d
liupeng <liupengfun@163.com>
2017-06-15 22:18:26 +0800

    项目可以启动

--- src/main/java/com.mello.spring/controller/TestController.java
+++ src/main/java/com.mello.spring/controller/TestController.java
@@ -1,5 +1,11 @@
 package com.spring.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
 /**
  * Created by lenovo on 2017/6/14.
  */
@@ -12,6 +18,5 @@ public class TestController {
         model.addAttribute("message", "Hello world!");
         //返回hello，则视图解析器会自动加前后缀，最后解析到/WEB-INF/pages/hello.jsp
         return "hello";
-
     }
 }
