e5f977915f9bf719723c774cdea36432d19962a4
liupeng <liupengfun@163.com>
2017-06-28 11:31:44 +0800

    add hibernate

---
+++ src/main/java/com/mello/spring/entity/News.java
@@ -0,0 +1,43 @@
+package com.mello.spring.entity;
+
+/**
+ * Created by liupeng on 2017/6/28.
+ */
+public class News {
+    private int id;
+    private String title;
+    private String author;
+    private String date;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
